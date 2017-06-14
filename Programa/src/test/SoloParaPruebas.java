1fb9c788b2337d6385dc63aa8b120c2a54cf782a
Victor-UNLa <josevictoribanez@gmail.com>
2017-06-14 10:50:23 -0300

    modifique hibernate

--- Programa/src/test/SoloParaPruebas.java
+++ Programa/src/test/SoloParaPruebas.java
@@ -1,13 +1,10 @@
 package test;
 
-import datos.*;
-import negocio.*;
 
 public class SoloParaPruebas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("test");
 	}
 
 }
